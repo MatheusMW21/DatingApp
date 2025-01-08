fd66aed059323d8e90f8c487cf35932020a54e13
MatheusMW21 <matheusmw21@gmail.com>
2025-01-08 10:23:13 -0300

    Adding a API Controller

--- demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb0a3950030857f1f3ec5c061ed0a2cf37bec5c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfafc7b720c5112acad25093ae74ca21d54e6d2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
