f0d3961ab1061826a208e64716f76514cbc1f8a9
MatheusMW21 <matheusmw21@gmail.com>
2025-01-11 14:02:12 -0300

    angular features

--- demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fbd8ab03013038c1036f45684f6b7c36d8694335")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+550d36fb79d30e005168dc5618d986e6b4064aa4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
