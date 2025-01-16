375057cc9965977618a77cf17a7c1adc0b5dea33
MatheusMW21 <matheusmw21@gmail.com>
2025-01-15 21:29:11 -0300

    register and login features

--- demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7804db7ba2c11353451b114f6663d4ff8105ad1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1754a53fbfb5e2b5897861cd38b40bf235cd7f5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
