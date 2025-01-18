d65583ef87545bed7da40ac6fbb1a10217ff0d20
MatheusMW21 <matheusmw21@gmail.com>
2025-01-18 11:12:18 -0300

    feat: errors handling

--- demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f96797fb14a7bdcce9672038e626500eeecbdd9f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cee81031d1eff7c7954cccf6f6c7cd87aa1e7cf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
