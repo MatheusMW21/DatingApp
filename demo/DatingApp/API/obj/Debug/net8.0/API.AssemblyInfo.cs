a88492cc305598e588579d26dde37e74193a8a60
MatheusMW21 <matheusmw21@gmail.com>
2025-02-10 19:03:29 -0300

    feat: adding a member service

--- demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ demo/DatingApp/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c35e53b6572c00dc2d3ef7bf0f726aed1dba68d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4207bd8923dcae5318984948764fe9329377c33c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
